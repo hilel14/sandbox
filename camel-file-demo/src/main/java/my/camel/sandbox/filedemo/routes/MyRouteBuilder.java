0a0bf8d324231c66d61535526c9e50f2b7f108e3
c7 <c7@localhost>
2015-02-13 06:33:50 +0200

    Use Camel with Property Inject annotation

--- camel-file-demo/src/main/java/my/camel/sandbox/filedemo/routes/MyRouteBuilder.java
+++ camel-file-demo/src/main/java/my/camel/sandbox/filedemo/routes/MyRouteBuilder.java
@@ -2,6 +2,7 @@ package my.camel.sandbox.filedemo.routes;
 
 import my.camel.sandbox.filedemo.model.Product;
 import my.camel.sandbox.filedemo.processors.MyProcessor;
+import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.BindyType;
 
@@ -12,10 +13,13 @@ import org.apache.camel.model.dataformat.BindyType;
  */
 public class MyRouteBuilder extends RouteBuilder {
 
+    @PropertyInject("inputFolder")
+    private String inputFolder;
+
     @Override
     public void configure() throws Exception {
 
-        from("file:///var/opt/data/in")
+        from(inputFolder)
                 .routeId("my-route-1")
                 .split(body().tokenize("\n")).streaming()
                 .choice().when(header("CamelSplitIndex").isEqualTo(0)).log("do some setup").end()
