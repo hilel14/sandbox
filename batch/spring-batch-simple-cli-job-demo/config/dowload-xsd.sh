bbd072e60563c593a9b23fb65344a8126d39e5b2
hilel <hilel@hilel.ebill.co.il>
2015-03-15 16:48:43 +0200

    spring-batch-simple-cli-job-demo: add script to download xsd schema files for offline use (printdomain)

---
+++ batch/spring-batch-simple-cli-job-demo/config/dowload-xsd.sh
@@ -0,0 +1,6 @@
+wget http://www.springframework.org/schema/beans/spring-beans.xsd
+wget http://www.springframework.org/schema/batch/spring-batch.xsd
+wget http://www.springframework.org/schema/context/spring-context.xsd
+wget http://www.springframework.org/schema/util/spring-util.xsd
+wget http://www.springframework.org/schema/jdbc/spring-jdbc.xsd
+wget http://www.springframework.org/schema/tool/spring-tool.xsd
