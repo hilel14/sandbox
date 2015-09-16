012f60d9eaa1b52e6d81a2456ccac44c64970074
Hilel New <hilel14@gmail.com>
2015-09-17 00:00:22 +0300

    new project: camel-jaxb-demo

---
+++ camel/camel-jaxb-demo/src/main/java/com/mycompany/jaxbdemo/model/Person.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jaxbdemo.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author c7
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Person  {
+
+    @XmlElement
+    private String firstName;
+
+    @XmlAttribute
+    private String user;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
