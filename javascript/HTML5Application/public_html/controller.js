a4f3a15c593c5415399c6cec421c2a3a341654a4
hilel <hilel@localhost>
2015-10-21 12:28:30 +0300

    separate between controller and persistence

--- javascript/HTML5Application/public_html/controller.js
+++ javascript/HTML5Application/public_html/controller.js
@@ -29,13 +29,13 @@ window.addEventListener("load", function () {
         deleteButtonOnClick();
     });
     // start with all-contacts view
-    fillAddressList();
+    populateContactTable();
     showSelectedPanel("list-panel");
     showSelectedButtons(["all-button", "create-button"]);
 });
 
 function allButtonOnClick() {
-    fillAddressList();
+    populateContactTable();
     showSelectedPanel("list-panel");
     showSelectedButtons(["all-button", "create-button"]);
 }
@@ -58,18 +58,52 @@ function createButtonOnClick() {
 }
 
 function saveButtonOnClick() {
-    saveContact();
-    fillAddressList();
+    var form = document.getElementById("contact-details-form");
+
+    var contact = {
+        first_name: form.elements["firstName"].value,
+        last_name: form.elements["lastName"].value,
+        phone: form.elements["phone"].value,
+        email: form.elements["email"].value
+    };
+
+    var contactId = form.elements["contactId"].value;
+    if (contactId === "") {
+        createContact(contact);
+    } else {
+        contact.id = contactId;
+        updateContact(contact);
+    }
+
+    populateContactTable();
     showSelectedPanel("list-panel");
     showSelectedButtons(["all-button", "create-button"]);
 }
 
 function deleteButtonOnClick() {
-    deleteContact();
+    var form = document.getElementById("contact-details-form");
+    var contactId = form.elements["contactId"].value;
+    deleteContact(contactId);
+
+    populateContactTable();
+    showSelectedPanel("list-panel");
+    showSelectedButtons(["all-button", "create-button"]);
+}
+
+function editButtonOnClick(contact) {
+    var form = document.getElementById("contact-details-form");
+    form.elements["contactId"].value = contact.id;
+    form.elements["firstName"].value = contact.first_name;
+    form.elements["lastName"].value = contact.last_name;
+    form.elements["phone"].value = contact.phone;
+    form.elements["email"].value = contact.email;
+
+    showSelectedPanel("edit-panel");
+    showSelectedButtons(["cancel-button", "save-button", "delete-button"]);
 }
 
 /*
- * Utility functions
+ * Content manipulation functions
  */
 
 function showSelectedPanel(selectedPanel) {
@@ -99,65 +133,10 @@ function showMessage(msg) {
     showSelectedButtons(["all-button"]);
 }
 
-/*
- * CRUD operations
- */
-
-function saveContact() {
-    var form = document.getElementById("contact-details-form");
-    var contactId = form.elements["contactId"].value;
-    if (contactId === "") {
-        contactId = addressList.length > 0 ? addressList[addressList.length - 1].id + 1 : 101;
-        // create and push new object to addressList
-        var newContact = {id: contactId, first_name: "", last_name: "", phone: "", email: ""};
-        addressList.push(newContact);
-    }
-    var selectedContact = findContactById(contactId);
-    selectedContact.first_name = form.elements["firstName"].value;
-    selectedContact.last_name = form.elements["lastName"].value;
-    selectedContact.phone = form.elements["phone"].value;
-    selectedContact.email = form.elements["email"].value;
-}
-
-function editContact(contactId) {
-    var form = document.getElementById("contact-details-form");
-    form.elements["contactId"].value = contactId;
-    //document.getElementById("formContactId").innerHTML = contactId;
-    var contact = findContactById(contactId);
-    //document.getElementById("formFirstName").innerHTML = contact.first_name;
-    form.elements["firstName"].value = contact.first_name;
-    form.elements["lastName"].value = contact.last_name;
-    form.elements["phone"].value = contact.phone;
-    form.elements["email"].value = contact.email;
-
-    showSelectedPanel("edit-panel");
-    showSelectedButtons(["cancel-button", "save-button", "delete-button"]);
-}
-
-function deleteContact() {
-    if (addressList.length < 2) {
-        showMessage("Can't delete the last address");
-        return;
-    }
-    var form = document.getElementById("contact-details-form");
-    var contactId = form.elements["contactId"].value;
-    for (i = 0; i < addressList.length; i++) {
-        if (addressList[i].id == contactId) {
-            //alert(i);
-            addressList.splice(i, 1);
-            break;
-        }
-    }
-
-    fillAddressList();
-    showSelectedPanel("list-panel");
-    showSelectedButtons(["all-button", "create-button"]);
-}
-
-function fillAddressList() {
+function populateContactTable() {
     //"use strict";
     //window.alert(new Date());
-    var table = document.getElementById("address-list-table");
+    var table = document.getElementById("contact-table");
     var tbody = table.getElementsByTagName("tbody")[0];
     var master = tbody.getElementsByTagName("tr")[0].cloneNode(true);
 
@@ -165,10 +144,12 @@ function fillAddressList() {
         tbody.deleteRow(i - 1);
     }
 
-    var n = addressList.length;
+    var contactList = readAllContacts();
+    var n = contactList.length;
+
     for (i = 0; i < n; i++) {
         var row = master.cloneNode(true);
-        fillOneRow(row, i);
+        fillOneRow(row, contactList[i]);
         tbody.appendChild(row);
     }
 
@@ -177,47 +158,15 @@ function fillAddressList() {
     td2.innerHTML = new Date();
 }
 
-function fillOneRow(row, i) {
+function fillOneRow(row, input) {
     //window.alert(i);
     var cells = row.getElementsByTagName("td");
-    cells[0].innerHTML = addressList[i].id;
+    cells[0].innerHTML = input.id;
     cells[0].addEventListener("click", function () {
-        editContact(addressList[i].id);
+        editButtonOnClick(input);
     });
-    cells[1].innerHTML = addressList[i].first_name;
-    cells[2].innerHTML = addressList[i].last_name;
-    cells[3].innerHTML = addressList[i].phone;
-    cells[4].innerHTML = addressList[i].email;
+    cells[1].innerHTML = input.first_name;
+    cells[2].innerHTML = input.last_name;
+    cells[3].innerHTML = input.phone;
+    cells[4].innerHTML = input.email;
 }
-
-function findContactById(contactId) {
-    for (i = 0; i < addressList.length; i++) {
-        if (addressList[i].id == contactId) {
-            return addressList[i];
-        }
-    }
-}
-
-var addressList = [
-    {
-        id: 101,
-        first_name: "John",
-        last_name: "Smith",
-        phone: "111 2222",
-        email: "john.smith@example.com"
-    },
-    {
-        id: 102,
-        first_name: "Johnny",
-        last_name: "Boy",
-        phone: "333 4444",
-        email: "johnny.boy@example.org"
-    },
-    {
-        id: 103,
-        first_name: "Johanna",
-        last_name: "Jones",
-        phone: "555 6666",
-        email: "johanna.jones@example.net"
-    }
-];
