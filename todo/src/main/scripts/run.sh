0238b1bb90c8e17c490be8b4d576068f0b269e0a
Hilel New <hilel14@gmail.com>
2015-10-24 09:44:24 +0300

    delete todo project (swing gui, with bugs)

--- todo/src/main/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-APP_HOME=`dirname $0`
-
-java -classpath $APP_HOME/resources/:$APP_HOME/lib/* beeriprint.todo.gui.MainFrame
--- todo/src/main/scripts/start-h2-console.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -cp ~/.m2/repository/com/h2database/h2/1.4.187/h2-1.4.187.jar org.h2.tools.Console
