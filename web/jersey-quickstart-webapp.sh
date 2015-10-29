c9653a50d311c41a14f7c8353003a9d80451a40c
Hilel New <hilel@localhost.localdomain>
2015-10-29 16:25:36 +0200

    add jersey maven artifact script

---
+++ web/jersey-quickstart-webapp.sh
@@ -0,0 +1,8 @@
+mvn archetype:generate \
+	-DarchetypeArtifactId=jersey-quickstart-webapp \
+	-DarchetypeGroupId=org.glassfish.jersey.archetypes \
+	-DarchetypeVersion=2.22.1 \
+	-DinteractiveMode=false \
+	-DgroupId=com.example \
+	-DartifactId=simple-service-webapp \
+	-Dpackage=com.example
