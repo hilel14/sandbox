03c2e965ea9c2b7f1c798f54fe87d6496c9cc428
Hilel Y <hilel@beeriprint.co.il>
2017-01-31 12:10:47 +0200

    Add swing sample application

---
+++ swing/sample-application/src/main/java/org/hilel14/swing/sample/application/FileProcessor.java
@@ -0,0 +1,79 @@
+package org.hilel14.swing.sample.application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hilel-deb
+ */
+public class FileProcessor {
+
+    static final Logger LOGGER = Logger.getLogger(FileProcessor.class.getName());
+    ProgressListener listener;
+    Charset charset;
+
+    public FileProcessor(ProgressListener listener) throws IOException {
+        this.listener = listener;
+        setup();
+    }
+
+    private void setup() throws IOException {
+        InputStream in = FileProcessor.class.getClassLoader().getResourceAsStream("io.properties");
+        Properties props = new Properties();
+        props.load(in);
+        charset = Charset.forName(props.getProperty("charset"));
+    }
+
+    public void processFile(Path inFile, Path outFile) throws IOException {
+        LOGGER.log(Level.INFO, "input file = {0}", inFile);
+        listener.showMessage("analyzing " + inFile);
+        int totalLines = countLines(inFile);
+        listener.showMessage("processing " + inFile);
+        int currentLine = 0;
+        String line;
+        try (BufferedReader reader = Files.newBufferedReader(inFile, charset);
+                BufferedWriter writer = Files.newBufferedWriter(outFile, charset, StandardOpenOption.CREATE)) {
+            while ((line = reader.readLine()) != null) {
+                currentLine++;
+                listener.showProgress((int) (currentLine * 100 / totalLines));
+                String data = porcessLine(line);
+                writer.write(data);
+                writer.newLine();
+            }
+        }
+        listener.showMessage("The operation completed successfully");
+        LOGGER.log(Level.INFO, "The operation completed successfully");
+    }
+
+    private int countLines(Path inFile) throws IOException {
+        int totalLines = 0;
+        try (BufferedReader reader = Files.newBufferedReader(inFile, charset);) {
+            while ((reader.readLine()) != null) {
+                totalLines++;
+            }
+        }
+        return totalLines;
+    }
+
+    private String porcessLine(String line) {
+        /*
+         try {
+         Thread.sleep(1);
+         } catch (InterruptedException ex) {
+         Logger.getLogger(FileProcessor.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         */
+        return line.toUpperCase();
+    }
+
+}
---
+++ swing/sample-application/src/main/java/org/hilel14/swing/sample/application/Main.java
@@ -0,0 +1,45 @@
+package org.hilel14.swing.sample.application;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hilel
+ */
+public class Main implements ProgressListener {
+
+    static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Mandatory arguments: input output");
+            System.exit(1);
+        }
+        Path inFile = Paths.get(args[0]);
+        Path outFile = Paths.get(args[1]);
+        try {
+            FileProcessor processor = new FileProcessor(new Main());
+            processor.processFile(inFile, outFile);
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+            System.out.println(ex.toString());
+        }
+    }
+
+    @Override
+    public void showProgress(int percent) {
+        //System.out.println(percent + "% completed");
+    }
+
+    @Override
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+}
