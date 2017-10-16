379976a9f0e9e2c32c4ad96aca224de7e2fb66d5
hilel14 <hilel14@gmail.com>
2017-10-16 09:15:17 +0300

    add to devops docker container

--- docker/devops/git/git.setup.sh
+++ docker/devops/git/git.setup.sh
@@ -4,10 +4,13 @@
 yum -y install git
 yum clean all
 
-# enable services
-systemctl enable httpd.service
-
 # Configure git over http
-mkdir /var/www/git
+mkdir /var/lib/tomcat/git
 cd /root/git
 mv git.httpd.conf /etc/httpd/conf.d/git.conf
+
+# Post build tasks:
+
+# htpasswd -c /var/lib/tomcat/git/.htpasswd user1
+# git init --bare /var/lib/tomcat/git/project-one.git
+# chown -R apache /var/lib/tomcat/git/project-one.git
